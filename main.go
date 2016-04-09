50a08363578f3ba1529471c590ef6b48e4e3a62d
maresladen <reily0308@gmail.com>
2016-04-09 14:49:37 +0800

    初次提交

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	_ "myproject/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	// 设置静态文件地址
+    // StaticDir["/static"] = "static"
+	// beego.SetStaticPath("/down1", "download1")
+	// beego.SetStaticPath("/down2", "download2")
+    
+    //设置session开启
+    
+	beego.Run()
+    
+    
+
+}
---
+++ static/js/test.js
@@ -0,0 +1,4 @@
+
+function addLevel() {
+    $("#mContent").append("<div><label>地址描述</label><input></input><label>输入地址</label>        <input></input></div>")
+}
